961f9b4dc769e68d8702f3be20465e4fc3052331
Yusuf Özgül <yusuf.ozgul@trendyol.com>
2021-12-26 21:07:11 +0300

    Initial Commit

---
+++ SyncFocusWithSlack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SyncFocusWithSlack
+//
+//  Created by Yusuf Özgül on 26.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SyncFocusWithSlack/SyncFocusWithSlackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncFocusWithSlackApp.swift
+//  SyncFocusWithSlack
+//
+//  Created by Yusuf Özgül on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SyncFocusWithSlackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
